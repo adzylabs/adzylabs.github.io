a8bf0ae31fcdf32cd0b75af22ad03bada08e8eac
adzylabs <post2mhto@gmail.com>
2025-02-03 19:30:35 +0530

    manner

--- components/pagetitle/PageTitle.js
+++ components/pagetitle/PageTitle.js
@@ -3,12 +3,12 @@ import React from 'react'
 
 const PageTitle = (props) => {
     return (
-        <section className="page_banner_section text-center" style={{ backgroundImage: `url(${'/images/shapes/bg_pattern_4.svg'})` }}>
+        <section className="page_banner_section text-center" style={{ backgroundImage: `url(${'/images/backgrounds/bg.jpg'})` }}>
             <div className="container">
-                <div className="heading_focus_text text-white">
+        {/*       <div className="heading_focus_text text-white">
                     {props.pageTop}
                     <span className="badge bg-secondary">{props.pagesub}</span>
-                </div>
+                </div> */}
                 <h1 className="page_title mb-0 text-white">{props.pageTitle}</h1>
             </div>
         </section>
